6b795e0a5f329f3dfb6c008fbb3b1cc22e5cf939
Marcelo Mendes <maeceloacm1998@gmail.com>
2025-03-10 23:08:42 -0300

    chore: add documentation to bottomsheet and snackbar component

--- app/src/main/java/com/app/comu_carona/components/snackbar/CCSnackbar.kt
+++ app/src/main/java/com/app/comu_carona/components/snackbar/CCSnackbar.kt
@@ -3,29 +3,34 @@ package com.app.comu_carona.components.snackbar
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarDefaults.contentColor
-import androidx.compose.material3.SnackbarDefaults.shape
+import androidx.compose.material3.SnackbarDefaults
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+/**
+ * A custom Snackbar component.
+ *
+ * @param modifier Modifier to be applied to the Snackbar.
+ * @param snackbarHostState State of the Snackbar host.
+ * @param snackbarType Type of the Snackbar to determine its appearance.
+ */
 @Composable
 fun CCSnackbar(
     modifier: Modifier = Modifier,
     snackbarHostState: SnackbarHostState,
     snackbarType: SnackbarCustomType = SnackbarCustomType.SUCCESS,
 ) {
-    Box(modifier = modifier) {
+    Box(modifier = modifier.padding(16.dp)) {
         SnackbarHost(
             hostState = snackbarHostState,
             snackbar = { data ->
                 Snackbar(
-                    modifier = Modifier.padding(horizontal = 16.dp),
                     containerColor = snackbarType.background,
-                    contentColor = contentColor,
-                    shape = shape,
+                    contentColor = SnackbarDefaults.contentColor,
+                    shape = SnackbarDefaults.shape,
                     actionOnNewLine = false,
                     snackbarData = data
                 )
