578fc7857fd19ce0a752b98f8f6f6f09a63f2744
Marcelo Mendes <maeceloacm1998@gmail.com>
2025-03-17 00:07:03 -0300

    feat: add loading screen to my car ride

--- app/src/main/java/com/app/comu_carona/feature/myrideinprogressdetails/ui/MyRideInProgressDetailsScreenLoading.kt
+++ app/src/main/java/com/app/comu_carona/feature/myrideinprogressdetails/ui/MyRideInProgressDetailsScreenLoading.kt
@@ -1,4 +1,94 @@
 package com.app.comu_carona.feature.myrideinprogressdetails.ui
 
-class MyRideInProgressDetailsScreenLoading {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.app.comu_carona.R
+import com.app.comu_carona.components.button.CCButton
+import com.app.comu_carona.components.button.CCButtonBack
+import com.app.comu_carona.components.carridecard.AddressBoxShimmer
+import com.app.comu_carona.components.carridecard.UserSelectionBoxShimmer
+import com.app.comu_carona.components.horizontalline.HorizontalLine
+import com.app.comu_carona.theme.BackgroundSkeleton
+import com.valentinilk.shimmer.shimmer
+
+@Composable
+fun MyRideInProgressDetailsScreenLoading(
+    onBack: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(White)
+            .padding(24.dp)
+    ) {
+        Column(
+            modifier = Modifier.weight(2f)
+        ) {
+            Spacer(modifier = Modifier.height(20.dp))
+
+            CCButtonBack(onClick = onBack)
+
+            Spacer(modifier = Modifier.height(35.dp))
+
+            Column(
+                modifier = Modifier
+                    .shimmer()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(height = 30.dp, width = 200.dp)
+                        .background(BackgroundSkeleton)
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Box(
+                    modifier = Modifier
+                        .size(height = 30.dp, width = 120.dp)
+                        .background(BackgroundSkeleton)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AddressBoxShimmer()
+
+            HorizontalLine(modifier = Modifier.padding(vertical = 20.dp))
+
+            UserSelectionBoxShimmer()
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(White)
+        ) {
+            HorizontalLine(Modifier.padding(bottom = 20.dp))
+
+            CCButton(
+                Modifier.padding(bottom = 20.dp),
+                title = stringResource(R.string.my_ride_in_progress_details_cancel_bottom_sheet_confirm_button_title),
+                isEnable = false,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyRideInProgressDetailsScreenLoadingPreview() {
+    MyRideInProgressDetailsScreenLoading()
 }
